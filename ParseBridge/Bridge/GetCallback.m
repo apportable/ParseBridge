4cce87fac02a07688ebff199462b7fcfee14d6aa
Glenna Buford <glenna@apportable.com>
2013-09-06 17:53:31 -0700

    explicitly release stuff

--- ParseBridge/Bridge/GetCallback.m
+++ ParseBridge/Bridge/GetCallback.m
@@ -90,8 +90,9 @@
         error = [NSError errorWithDomain:[exception localizedMessage] code:[exception getCode] userInfo:nil];
     }
     dispatch_async(dispatch_get_main_queue(), ^{
-        PFObject *pfObject = [[[PFObject alloc] initWithParseObject:parseObject] autorelease];
+        PFObject *pfObject = [[PFObject alloc] initWithParseObject:parseObject];
         _handler(pfObject, error);
+        [pfObject release];
         [self autorelease]; // To balance the retain cycle created from the callbackWithHandler: method
     });
 }
