b7983e0085e4d34a1f4b62137a82d7389ace7732
Jeremy Cartier <jcartier@macbook-air.local>
2013-10-07 14:45:31 -0700

    Tab/space cleanup.

--- java/src/com/parsebridge/parsebridge/ParseBridgeTwitterUtilsShim.java
+++ java/src/com/parsebridge/parsebridge/ParseBridgeTwitterUtilsShim.java
@@ -9,16 +9,17 @@ import com.parsebridge.ParseBridgeLogInCallback;
 
 public class ParseBridgeTwitterUtilsShim {
 
-  private static final String TAG = "ParseBridgeTwitterUtilsShim";
+private static final String TAG = "ParseBridgeTwitterUtilsShim";
 
 public static void logIn(final Context context, final LogInCallback callback)
-  {
+{
     ThreadUtils.runOnUiThread(new Runnable() {
         @Override
         public void run() {
             Log.d(TAG, "login");
             ParseTwitterUtils.logIn(context, callback);
-      }
+        }
     });
-  }
+}
+
 }
